89f18b2c289867601971acf2929463741e7028f5
dmtihor <dmtihor@gmail.com>
2015-12-13 20:14:50 +0200

    Initial commit

---
+++ src/com/dmtihor/Dispatcher.java
@@ -0,0 +1,10 @@
+package com.dmtihor;
+
+/**
+ * Created by Adm on 12/13/2015.
+ */
+public class Dispatcher {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
